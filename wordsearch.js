399ed291da2474c8785bae74fca18b3cfb262f79
tessfbs <tessfbs@gmail.com>
2023-02-16 16:19:26 -0500

    done

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+    let temp = [];
+   
+    for (let R = 0; R < matrix[0].length; R++) {
+      temp.push([]);
+      for (let C = 0; C < matrix.length; C++) {
+        temp[R].push(matrix[C][R]);
+      }
     }
-}
-
-module.exports = wordSearch
+    return temp;
+  };
+  const searchEngine = (letters, word) => {
+    const matrixJoin = letters.map(ls => ls.join(''));
+  
+    for (l of matrixJoin) {
+      if (l.includes(word))
+        return true;
+    }
+  };
+     
+  
+  const wordSearch = (letters, word) => {
+    if (searchEngine(letters, word)) {
+      return true;
+    } else if (searchEngine(transpose(letters), word)) {
+      return true;
+    }
+    return false;
+  };
+  module.exports = wordSearch;
